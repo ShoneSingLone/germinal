210c9abfc0fa98a4658a4294d3aa076a0dd6546c
ShoneSingLone <ShoneSingLone@gmail.com>
2021-11-02 23:35:11 +0800

    update

--- local_modules/ui/xForm/itemRenders/Input.jsx
+++ local_modules/ui/xForm/itemRenders/Input.jsx
@@ -1,11 +1,8 @@
-import { Input } from "ant-design-vue";
-import { InputPassword } from "ant-design-vue/es/input";
-import { h } from "vue";
-import { getComponentSettings } from "../../common.js";
+import {Input} from "ant-design-vue";
+import {InputPassword} from "ant-design-vue/es/input";
+import {h} from "vue";
 
-export default (props) => {
-    debugger;
-    const [property, slots] = getComponentSettings(props);
+export default ({property, slots}) => {
     if (property.isPassword) {
         return h(InputPassword, property, slots);
     } else {
--- local_modules/ui/xForm/xItem.vue
+++ local_modules/ui/xForm/xItem.vue
@@ -1,15 +1,23 @@
 <script lang="jsx">
-import { defineComponent, useAttrs, h, mergeProps, computed } from "vue";
+import {defineComponent, useAttrs, h, mergeProps, computed} from "vue";
 import renders from "./itemRenders";
+import {vModel} from "../common";
 
 export default defineComponent({
   props: ["configs"],
-  setup(props, { attrs }) {
-    const CurrentFormItemRender = renders[props.configs.type] || renders.Input;
-    console.log("🚀:", "props", JSON.stringify(props, null, 2));
-    const labelVNode = computed(() => {
+  computed: {
+    componentSettings() {
+      console.log("componentSettings");
+      const configs = {...this.configs, ...this.$attrs};
+      const xItemProperties = ["infoTips", "rules", "slots"];
+      const property = _.merge({}, configs, vModel(configs));
+      const slots = property.slots || {};
+      _.each(xItemProperties, prop => delete property[prop]);
+      return {property, slots};
+    },
+    labelVNode() {
       let label = (() => {
-        const _label = props.configs.label;
+        const _label = this.configs.label;
         if (_.isFunction(_label)) {
           return _label();
         }
@@ -19,17 +27,37 @@ export default defineComponent({
         return false;
       })();
 
-      if (_.isBoolean(label)) {
+      if (label === false) {
         return null;
       }
       return <label>{label}</label>;
-    });
-    return () => (
-      <>
-        {labelVNode.value}
-        <CurrentFormItemRender {...props.configs} {...attrs} />
-      </>
-    );
+    }
   },
+  render(h) {
+    const CurrentFormItemRender = renders[this.configs.type] || renders.Input;
+    return (
+        <>
+          <div class="ant-row ant-form-item ant-form-item-has-error ant-form-item-with-help" style="row-gap: 0px;">
+            <div class="ant-col ant-col-4 ant-form-item-label">
+              <label html-for="form_item_name" class="ant-form-item-required" title="Activity name">
+                Activity name
+              </label>
+            </div>
+            <div class="ant-col ant-col-14 ant-form-item-control">
+              <div class="ant-form-item-control-input">
+                <div class="ant-form-item-control-input-content">
+                  <input type="text" id="form_item_name" class="ant-input"/>
+                </div>
+              </div>
+              <div class="ant-form-item-explain ant-form-item-explain-error">
+                <div role="alert">Please input Activity name</div>
+              </div>
+            </div>
+          </div>
+          {this.labelVNode}
+          <CurrentFormItemRender {...this.componentSettings} />
+        </>
+    );
+  }
 });
 </script>
