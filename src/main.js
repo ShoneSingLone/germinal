f0fa495ec4396a49ccbc3fcc643912712785ed36
ShoneSingLone <ShoneSingLone@gmail.com>
2022-01-16 22:20:06 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { appPlugins } from "lsrc/utils/common";
 
-if (import.meta.env.MODE === "development") {
-	import("germinal_api/mock");
-}
+import("germinal_api/mock");
+// if (import.meta.env.MODE === "development") {
+// 	import("germinal_api/mock");
+// }
 
 createApp(App).use(appPlugins).mount("#app");
