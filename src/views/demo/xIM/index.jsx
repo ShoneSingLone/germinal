746b482b5c61844111546d6fba6a8442b22d3847
ShoneSingLone <ShoneSingLone@gmail.com>
2022-03-08 21:42:21 +0800

    layer

--- src/views/demo/xIM/index.jsx
+++ src/views/demo/xIM/index.jsx
@@ -1,7 +1,5 @@
 import { defineComponent } from "vue";
-import $ from "jquery";
-import { io } from "socket.io-client";
-import { lStorage } from "@ventose/ui";
+import { lStorage, _ } from "@ventose/ui";
 import { STATIC_WORD } from "lsrc/utils/common.words";
 
 export default defineComponent({
@@ -12,18 +10,9 @@ export default defineComponent({
 		const socket_url = `${window.URL_WS_BASE}?token=${
 			lStorage[STATIC_WORD.ACCESS_TOKEN]
 		}`;
-
-		// Create WebSocket connection.
 		const socket = new WebSocket(socket_url);
-
-		// Connection opened
-		// socket.addEventListener("open", function (event) {
-		// 	socket.send("Hello Server!");
-		// });
-
-		// Listen for messages
 		socket.addEventListener("message", function (event) {
-			console.log("Message from server ", event.data);
+			console.log("Message from server ", _.safeParse(event.data));
 		});
 	},
 	render(h) {
